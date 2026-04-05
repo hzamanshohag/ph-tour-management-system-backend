5949e0db4b9a8bce0414981bfc6ec46ec810533d
Md Hasanuzzaman Shohag <hzaman.live@gmail.com>
2026-04-06 05:04:54 +0600

    added bcrypt.js

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -2,9 +2,10 @@ import AppError from "../../ErrorHelpers/AppError";
 import { IAuthProvider, IUser } from "./user.interface";
 import { User } from "./user.model";
 import { StatusCodes } from "http-status-codes";
+import bcryptjs from "bcryptjs";
 
 const createUser = async (payload: Partial<IUser>) => {
-  const { email, ...rest } = payload;
+  const { email, password, ...rest } = payload;
   if (!email) {
     throw new Error("Email is required");
   }
@@ -15,10 +16,13 @@ const createUser = async (payload: Partial<IUser>) => {
     throw new AppError(StatusCodes.BAD_REQUEST, "This user are already exist");
   }
 
+  const hashedPassword = await bcryptjs.hash(password as string, 10);
+
   const authProvider: IAuthProvider = { provider: "google", providerId: email };
 
   const user = await User.create({
     email,
+    password: hashedPassword,
     auths: [authProvider],
     ...rest,
   });
